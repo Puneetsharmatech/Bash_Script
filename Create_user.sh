f5e8bf4fc9a28007c33015cc6289abcfa2cf4563
puneetsharmatech <Puneetsharma201120@gmail.com>
2025-02-14 02:02:04 -0500

    adding comment in code

--- Create_user.sh
+++ Create_user.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Asking for the username
 echo "Enter Your Name: "
 read  username
 
+
+# Checking if username already exists
 if id "$username" &>/dev/null; then
     echo "User $username already exists"
     exit 1
 fi
 
+#Asking for the password
 echo "Enter Your Password: "
 read -s userpass
 
@@ -15,7 +19,7 @@ echo "Enter Your Password again: "
 read -s userpass_again
 
 
-
+# Check if password matches then create a new user and set the password
 if [ "$userpass" != "$userpass_again" ]; then
     echo "Passwords do not match. Exiting..."
     exit 1
